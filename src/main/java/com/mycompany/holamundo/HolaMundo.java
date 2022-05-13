7b5502b55a3df499e4124bc540d8bc444e25cc75
Jose Guedes <clonestroper@gmail.com>
2022-05-12 20:23:16 -0400

    Programa HolaMundo

---
+++ src/main/java/com/mycompany/holamundo/HolaMundo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.holamundo;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author clone
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Muchachos, -_-");
+        System.out.println("Holisssss");
+    }
+}
